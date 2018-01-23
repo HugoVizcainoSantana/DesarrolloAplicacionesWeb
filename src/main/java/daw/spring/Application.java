cdcb4c6bc30e51312390373435104571ad29ff37
Hgmoa <hugo.97.vk@gmail.com>
2018-01-23 10:02:14 +0100

    Setting up empty spring boot project
    Signed-off-by: Hgmoa <hugo.97.vk@gmail.com>

---
+++ src/main/java/daw/spring/Application.java
@@ -0,0 +1,13 @@
+package daw.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Entry point
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
