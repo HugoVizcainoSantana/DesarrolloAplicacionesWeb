598bf3a722001b2694e4edfa7285786042a1c640
CVelazquezURJC <c.velazquezs@alumnos.urjc.es>
2018-02-06 11:23:49 +0100

    create UserClient model

---
+++ src/main/java/daw/spring/model/UserClient.java
@@ -0,0 +1,85 @@
+package daw.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class UserClient {
+
+    @id
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+
+
+    //
+    @OneToMany()
+    private List<Home> homeList;
+    //
+
+    public UserClient(){
+
+    }
+
+    public UserClient(long id, String firstName, String lastName, String email, String password, List<Home> homeList) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.homeList = homeList;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Home> getHomeList() {
+        return homeList;
+    }
+
+    public void setHomeList(List<Home> homeList) {
+        this.homeList = homeList;
+    }
+}
+
