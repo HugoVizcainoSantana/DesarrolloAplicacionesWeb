16a91ba44d8ebc9d6fd58e168c54d779c1665f05
Jorge Galindo Peña <jorgape225@gmail.com>
2018-02-08 14:19:27 +0100

    Changed analytics

--- src/main/java/daw/spring/model/Analytics.java
+++ src/main/java/daw/spring/model/Analytics.java
@@ -2,14 +2,15 @@ package daw.spring.model;
 
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import java.util.List;
+import javax.persistence.Id;
+import java.util.ArrayList;
 
 @Entity
 public class Analytics {
 
     @Id
     private long id;
+
     private AnalyticsType type;
     private String title;
     private String description;
@@ -21,11 +22,15 @@ public class Analytics {
     private List<User> userAnalytics;
     private List<Home> homeAnalytics;*/
 
+    private ArrayList<String> domain; // And a domain in Years, Months, Weeks or Days
 
+    private ArrayList<Integer> data; // And the data itself with the average
+    private ArrayList<Integer> dataAverage;
 
     public Analytics() {
     }
 
+    public Analytics(long id, AnalyticsType type, String title, String description, ArrayList<String> domain, ArrayList<Integer> data, ArrayList<Integer> dataAverage) {
         this.id = id;
         this.type = type;
         this.title = title;
@@ -67,21 +72,27 @@ public class Analytics {
         this.description = description;
     }
 
+    public ArrayList<String> getDomain() {
         return domain;
     }
 
+    public void setDomain(ArrayList<String> domain) {
         this.domain = domain;
     }
 
+    public ArrayList<Integer> getData() {
         return data;
     }
 
+    public void setData(ArrayList<Integer> data) {
         this.data = data;
     }
 
+    public ArrayList<Integer> getDataAverage() {
         return dataAverage;
     }
 
+    public void setDataAverage(ArrayList<Integer> dataAverage) {
         this.dataAverage = dataAverage;
     }
 
