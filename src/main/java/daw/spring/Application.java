a5ca1284ba7fcbac695eaf5068dccd280abd7c2b
Hgmoa <hugo.97.vk@gmail.com>
2018-02-08 14:45:03 +0100

    Added default error tag for MUstache

--- src/main/java/daw/spring/Application.java
+++ src/main/java/daw/spring/Application.java
@@ -1,7 +1,11 @@
 package daw.spring;
 
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 
 //Entry point
@@ -13,4 +17,15 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public Mustache.Compiler mustacheCompiler(Mustache.TemplateLoader templateLoader, Environment environment) {
+
+        MustacheEnvironmentCollector collector = new MustacheEnvironmentCollector();
+        collector.setEnvironment(environment);
+
+        return Mustache.compiler()
+                .defaultValue("### ERROR THIS IS A DEFAULT TAG ###")
+                .withLoader(templateLoader)
+                .withCollector(collector);
+    }
 }
