22af9f617bed90ee2fb4d81d05365db9b52914ee
DaniMaci <d.maciasm@alumnos.urjc.es>
2018-02-12 12:47:49 +0100

    new admin dashboard, new controler

--- src/main/java/daw/spring/controller/AdminDashboardController.java
+++ src/main/java/daw/spring/controller/AdminDashboardController.java
@@ -1,4 +1,33 @@
 package daw.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/adminDashboard")
+
 public class AdminDashboardController {
+    @RequestMapping("/index")
+    public String index() {
+        return "adminDashboard/index";
+    }
+
+    @RequestMapping("/inventario")
+    public String inventario() {
+        return "adminDashboard/inventario";
+    }
+
+
+    @RequestMapping("/usuarios")
+    public String usuarios() {
+        return "adminDashboard/usuarios";
+    }
+
+    @RequestMapping("/pedidos")
+    public String pedidos() {
+        return "adminDashboard/pedidos";
+    }
+
+
 }
