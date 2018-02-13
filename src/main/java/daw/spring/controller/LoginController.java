773b7d3ccda5751ac20d572379598ae942b5b694
Hgmoa <hugo.97.vk@gmail.com>
2018-02-13 12:09:26 +0100

    Fixed paths for spring on public site

--- src/main/java/daw/spring/controller/LoginController.java
+++ src/main/java/daw/spring/controller/LoginController.java
@@ -10,4 +10,9 @@ public class LoginController {
     public String login() {
         return "login";
     }
+
+    @RequestMapping("/forgotPassword")
+    public String forgotPassword() {
+        return "forgotPassword";
+    }
 }
