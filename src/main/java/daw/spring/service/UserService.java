44c34ecbbea454f80059758d8814568576b0321b
Ramon <r.serranoro@alumnos.urjc.es>
2018-02-16 18:46:34 +0100

    APIRest weather, form shop, upload images
    Implementation of the apirest of the time with consultations to openweathermap.org, contact form in the store for the purchase of products, and the upload of images on the server

--- src/main/java/daw/spring/service/UserService.java
+++ src/main/java/daw/spring/service/UserService.java
@@ -1,7 +1,44 @@
 package daw.spring.service;
 
+
+
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import daw.spring.model.User;
+import daw.spring.repository.UserRepository;
+
 public class UserService {
 
     //metodos gets usuarios --llama al repositorio y ejecuta la accion
+	
+	
+	
+	@Autowired
+    UserRepository userRepository;
 
+
+    public User findOneById(Long id){
+        return userRepository.findOne(id);
+    }
+
+
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+
+
+   /* public void deleteHome(Home home){
+        homeRepository.delete(home);
+    }
+    public Page<Home> findAllHomePage(PageRequest pageRequest){
+        return homeRepository.findAll(pageRequest);
+    }*/
+
+
+   
 }
