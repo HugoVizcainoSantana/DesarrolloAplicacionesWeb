d5dd6b84fa82d51097454e6caf2b627b45efaa8e
Hgmoa <hugo.97.vk@gmail.com>
2018-02-19 13:26:15 +0100

    Changed autowired fields to autowired constructor

--- src/main/java/daw/spring/service/UserService.java
+++ src/main/java/daw/spring/service/UserService.java
@@ -1,28 +1,25 @@
 package daw.spring.service;
 
-import daw.spring.model.Home;
 import daw.spring.model.User;
-import daw.spring.repository.HomeRepository;
 import daw.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
 
 @Service
 public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User findOneById(Long id){
         return userRepository.findOne(id);
     }
-
 
     public void saveUser(User user){
         userRepository.save(user);
