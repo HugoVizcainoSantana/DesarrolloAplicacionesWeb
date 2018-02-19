7444d33bae9a85f62501d5df73751a7d93e47b74
Jorge Galindo Peña <jorgape225@gmail.com>
2018-02-19 13:24:57 +0100

    Added RegisterController
    Added RegisterController and redirect to Login

--- src/main/java/daw/spring/controller/RegisterController.java
+++ src/main/java/daw/spring/controller/RegisterController.java
@@ -1,14 +1,32 @@
 package daw.spring.controller;
 
+import daw.spring.model.Home;
+import daw.spring.model.User;
+import daw.spring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class RegisterController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/register")
-    public String register() {
-        return "register";
+    public String register(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String passwordHash) {
+        List<Home> homeList = new ArrayList<>();
+        User user = new User(firstName, lastName, email, passwordHash, homeList, Collections.singletonList("USER"));
+
+        userRepository.save(user);
+
+        return "login";
+
     }
 
 }
