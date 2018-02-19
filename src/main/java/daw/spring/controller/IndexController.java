ab5e4441217d9296d2606d73d77994e2db9ed74a
Hgmoa <hugo.97.vk@gmail.com>
2018-02-19 18:46:28 +0100

    Enabled travis cache and corrected small things from merge

--- src/main/java/daw/spring/controller/IndexController.java
+++ src/main/java/daw/spring/controller/IndexController.java
@@ -6,18 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class IndexController {
 
     @Autowired
     private ProductService productService;
-
-    private static final String FILES_FOLDER = ".\\src\\main\\resources\\static\\images";
-
-    private List<String> imageTitles = new ArrayList<>();
 
     @RequestMapping("/")
     public String index(Model model) {
