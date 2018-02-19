1c9faef0f8aee00919657197e11e4b56760700c9
Jorge Galindo Peña <jorgape225@gmail.com>
2018-02-19 12:26:21 +0100

    Revert "User pages fails merger"

--- src/main/java/daw/spring/model/User.java
+++ src/main/java/daw/spring/model/User.java
@@ -2,89 +2,37 @@ package daw.spring.model;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-
 import java.util.List;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
-    
-    @NotEmpty
     private String firstName;
-    
-    @NotEmpty
     private String lastName;
-    
-    @NotEmpty
-    @Email
     private String email;
-    
-    
-    @NotEmpty
-    private String direccion;
-    
-    
-    private int cp;
-    
-    
     private String password;
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Home> homeList;
-    
-    private String foto;
 
     public User(){
 
     }
 
-    public User(long id, String firstName, String lastName, String email, String password,String direccion, int cp, List<Home> homeList, String foto) {
+    public User(long id, String firstName, String lastName, String email, String password, List<Home> homeList) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.homeList = homeList;
-        this.direccion=direccion;
-        this.cp=cp;
-        this.foto=foto;
     }
 
-    public String getFoto() {
-		return foto;
-	}
-
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-
-	public int getCp() {
-		return cp;
-	}
-
-	public void setCp(int cp) {
-		this.cp = cp;
-	}
-
-	public long getId() {
+    public long getId() {
         return id;
     }
 
