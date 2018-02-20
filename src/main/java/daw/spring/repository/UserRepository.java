7956b369bb84cb490239c6a7b6df3d41f74c7073
DaniMaci <d.maciasm@alumnos.urjc.es>
2018-02-20 10:48:50 +0100

    user List for database

--- src/main/java/daw/spring/repository/UserRepository.java
+++ src/main/java/daw/spring/repository/UserRepository.java
@@ -4,4 +4,5 @@ import daw.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findAllByFirstName(String name);
 }
--- src/main/java/daw/spring/service/UserService.java
+++ src/main/java/daw/spring/service/UserService.java
@@ -1,40 +1,50 @@
 package daw.spring.service;
 
+import daw.spring.model.Home;
 import daw.spring.model.User;
+import daw.spring.repository.HomeRepository;
 import daw.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-
     @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    UserRepository userRepository;
 
-    public User findOneById(Long id) {
+
+    public User findOneById(Long id){
         return userRepository.findOne(id);
     }
 
-    public void saveUser(User user) {
+    public List<User> findAll() {return userRepository.findAll();}
+
+
+    public void saveUser(User user){
         userRepository.save(user);
     }
+
+    public User findAllUsersByFirstName(String name) { return userRepository.findAllByFirstName(name);}
+
+
 
    /* public void deleteHome(Home home){
         homeRepository.delete(home);
     }
-
+    
     public Page<Home> findAllHomePage(PageRequest pageRequest){
         return homeRepository.findAll(pageRequest);
     }*/
 
-    @PostConstruct
-    public void prueba() {
-        saveUser(new User("Pepe", "Lopez", "pepelopez@test.com", "1234", "c/falsa 123", 123456, null, null));
-    }
+
+  /*@PostConstruct
+    public void prueba (){
+       saveUser(new User(1, "Pepe","Lopez","", "a@a.com", "aaa", 0, null, null));
+   }*/
 }
