7ba83535596b4fc8600295b29643b6e7c490c283
Jorge Galindo Peña <jorgape225@gmail.com>
2018-02-20 22:28:34 +0100

    Added Product details
    Added Product details and resolved some bugs.

--- src/main/java/daw/spring/controller/ProductController.java
+++ src/main/java/daw/spring/controller/ProductController.java
@@ -1,10 +1,14 @@
 package daw.spring.controller;
 
+import daw.spring.model.Product;
 import daw.spring.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static daw.spring.model.Product.ProductType.LIGHT;
 
 @Controller
 public class ProductController {
@@ -24,6 +28,19 @@ public class ProductController {
     public String indexConProductos(Model model) {
         model.addAttribute("products", productService.findAllProducts());
         return "index";
+    }
+
+    @RequestMapping("index/product/{id}")
+    public String productDetail(Model model, @PathVariable long id){
+        Product currentProduct = productService.findOneById(id);
+
+        model.addAttribute("selectedProduct", currentProduct);
+
+        if(currentProduct.getType() == LIGHT){
+            return "products/bombillas";
+        } else {
+            return "products/persianas";
+        }
     }
 
 
