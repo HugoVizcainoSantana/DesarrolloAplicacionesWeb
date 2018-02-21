ba8f762738d17b8861d04ab69ca4f402b26ce454
Ramon <r.serranoro@alumnos.urjc.es>
2018-02-21 21:12:00 +0100

    Upload photo
    upload photo with other folder

---
+++ src/main/java/daw/spring/MvcConfig.java
@@ -0,0 +1,30 @@
+package daw.spring;
+
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter{
+	
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
+	
+	/*@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		super.addResourceHandlers(registry);
+		String resourcePath = Paths.get("upload").toAbsolutePath().toUri().toString();
+		log.info(resourcePath);
+		registry.addResourceHandler("/upload/**").addResourceLocations(resourcePath);
+	}*/
+
+
+	
+
+}
--- src/main/java/daw/spring/controller/UserDashboardController.java
+++ src/main/java/daw/spring/controller/UserDashboardController.java
@@ -2,31 +2,47 @@ package daw.spring.controller;
 
 import daw.spring.model.User;
 import daw.spring.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/dashboard")
 public class UserDashboardController {
-
+	
+	
     private final UserService userService;
+	
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     public UserDashboardController(UserService userService) {
-        this.userService = userService;
+       this.userService = userService;
     }
 
     @RequestMapping("/")
@@ -77,42 +93,73 @@ public class UserDashboardController {
         model.addAttribute("titulo", "Casa");
         return "dashboard/homes";
     }
+    
+    @GetMapping(value="/see/{id}")
+    public String seeDetail(@PathVariable(value="id") Long id, Map<String, Object> model) {
+    		User user = userService.findOneById(id);
+    		
+    		if (user==null) {
+    			return "dashboard/profile";
+    		}
+    		model.put("user", user);
+    		model.put("title", "Detalle cliente"+ user.getFirstName());
+    	
+    		return"dashboard/see";
+    	
+    }
+    
+    @GetMapping(value="/upload/{filename:.+}")
+    public ResponseEntity<Resource> seePhoto (@PathVariable String fileName){
+    		Path pathPhoto = Paths.get("upload").resolve(fileName).toAbsolutePath();
+    		log.info("Pathphoto: "+pathPhoto);
+    		Resource resource = null;
+    		try {
+    				resource = new UrlResource(pathPhoto.toUri());
+    				if(!resource.exists() || !resource.isReadable()) {
+    					throw new RuntimeException("Error no se ha podido cargar la imgen: "+ pathPhoto.toString());
+    				}
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION	, "attachment; filename=\""+resource.getFilename()+"\"").body(resource);
+    	
+    }
 
     @RequestMapping("/profile")
     public String profile(Model model) {
         User user = new User();
-        model.addAttribute("user", userService.findOneById(1l));
+        model.addAttribute("user", userService.findOneById(2l));
 
         return "dashboard/profile";
     }
+    
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(@Valid User user, BindingResult result, Model model, @RequestParam("file") MultipartFile photo, SessionStatus status) {
-        
-    		
-    		if (result.hasErrors()) {
-            return "dashboard/profile";
-        }
+    public String saveProfile(@Valid User user, BindingResult result, Model model, @RequestParam("file") MultipartFile photo) {
+    		/*if (result.hasErrors()) {
+    			return "dashboard/profile";
+        }*/
         if (!photo.isEmpty()) {
-        		//Path directorioRecusrsos=Paths.get("file");
-        
-            Path directorioRecusrsos = Paths.get("src//main//resources//static//upload");
-            String rootPath = directorioRecusrsos.toFile().getAbsolutePath();
+
+        		String uniqueFilname = UUID.randomUUID().toString()+"_"+photo.getOriginalFilename();
+        	
+        		Path rootPath= Paths.get("upload").resolve(uniqueFilname);
+        		Path rootAbsolutePath = rootPath.toAbsolutePath();
+        		log.info("rootPath: "+ rootPath);
+        		log.info("rootAbsolutePath: "+ rootAbsolutePath);
 
             try {
-                byte[] bytes = photo.getBytes();
-                Path rutaCompleta = Paths.get(rootPath + "//" + photo.getOriginalFilename());
-                Files.write(rutaCompleta, bytes);
-                //flash.addAttribute("info", "Ha subido correctamente '"+ foto.getOriginalFilename()+"'");
-                user.setPhoto(photo.getOriginalFilename());
+                Files.copy(photo.getInputStream(), rootAbsolutePath);
+               
+                user.setPhoto(uniqueFilname);
+                
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         userService.saveUser(user);
-        //model.addAttribute("titulo", "Perfil");
-        //status.setComplete();
-        return "dashboard/created";
+        return "dashboard/see";
     }
 
     @RequestMapping("/terms-Conditions")
