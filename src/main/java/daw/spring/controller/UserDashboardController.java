715786c11e446ac84da444c8376247560465e290
Hgmoa <hugo.97.vk@gmail.com>
2018-03-02 10:18:39 +0100

    Fixing ramon merge...

--- src/main/java/daw/spring/controller/UserDashboardController.java
+++ src/main/java/daw/spring/controller/UserDashboardController.java
@@ -5,8 +5,6 @@ import daw.spring.component.CurrentUserInfo;
 import daw.spring.component.InvoiceGenerator;
 import daw.spring.model.*;
 import daw.spring.service.*;
-
-//import org.apache.catalina.startup.HomesUserDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-
-//import static org.hamcrest.CoreMatchers.instanceOf;
-//import static org.mockito.Mockito.after;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
@@ -36,6 +30,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+
+//import org.apache.catalina.startup.HomesUserDatabase;
+//import static org.hamcrest.CoreMatchers.instanceOf;
+//import static org.mockito.Mockito.after;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -102,45 +100,52 @@ public class UserDashboardController implements CurrentUserInfo {
         index(model, principal);
     }
 
-	@RequestMapping("/shop")
-	public String shop(Model model, Principal principal) {
-		model.addAttribute("user", userService.findOneById(getIdFromPrincipalName(principal.getName())));
-		model.addAttribute("title", "Tienda");
-		return "dashboard/shop";
-	}
+    @RequestMapping("/shop")
+    public String shop(Model model, Principal principal) {
+        model.addAttribute("user", userService.findOneById(getIdFromPrincipalName(principal.getName())));
+        model.addAttribute("title", "Tienda");
+        return "dashboard/shop";
+    }
 
 
-	@RequestMapping (value="/shop", method = RequestMethod.POST)
-	public String addOrder (Principal principal, @RequestParam(name="direccion")String address,
-                            @RequestParam(name = "postCode") long postCode,
-                            @RequestParam(name = "blind") Integer blindQuantity,
-                            @RequestParam(name = "light") Integer lightQuantity) {
-		Double totalPrice = (double) (blindQuantity*20+lightQuantity*30);
-		log.info("Ha entrado en el metodo");
-		List<Device>deviceList= new ArrayList<>();
-		User user = userService.findOneById(getIdFromPrincipalName(principal.getName()));
-		for(int i=0; i<blindQuantity; i++) {
-			Device device = new Device("Actuador de persiana", 150, Device.DeviceType.BLIND, Device.StateType.UP, null, false, null,false);
-			deviceService.saveDevice(device);
-			deviceList.add(device);
-		}
-		for(int i=0; i<lightQuantity; i++) {
-			Device device = new Device("Actuador de bombilla", 30, Device.DeviceType.LIGHT, Device.StateType.ON, null, false, null,false);
-			deviceService.saveDevice(device);
-			deviceList.add(device);
-		}
-		log.info("Empieza a guardar");
-		Home home = new Home(postCode, address, false, deviceList );
-		homeService.saveHome(home);
-		userService.saveHomeUser(home, user);
+    @RequestMapping(value = "/shop", method = RequestMethod.POST)
+    public String addOrder(Principal principal,
+                           @RequestParam(name = "direccion") String address,
+                           @RequestParam(name = "postCode") long postCode,
+                           @RequestParam(name = "blind", required = false) Integer blindQuantity,
+                           @RequestParam(name = "light", required = false) Integer lightQuantity) {
+        if (blindQuantity == null) {
+            blindQuantity = 0;
+        }
+        if (lightQuantity == null) {
+            lightQuantity = 0;
+        }
+        Double totalPrice = (double) (blindQuantity * 20 + lightQuantity * 30);
+        log.info("Ha entrado en el metodo");
+        List<Device> deviceList = new ArrayList<>();
+        User user = userService.findOneById(getIdFromPrincipalName(principal.getName()));
+        for (int i = 0; i < blindQuantity; i++) {
+            Device device = new Device("Actuador de persiana", 150, Device.DeviceType.BLIND, Device.StateType.UP, null, false, null, false);
+            deviceService.saveDevice(device);
+            deviceList.add(device);
+        }
+        for (int i = 0; i < lightQuantity; i++) {
+            Device device = new Device("Actuador de bombilla", 30, Device.DeviceType.LIGHT, Device.StateType.ON, null, false, null, false);
+            deviceService.saveDevice(device);
+            deviceList.add(device);
+        }
+        log.info("Empieza a guardar");
+        Home home = new Home(postCode, address, false, deviceList);
+        homeService.saveHome(home);
+        userService.saveHomeUser(home, user);
         //Order order = new Order(total, false, home);
         OrderRequest order = new OrderRequest(totalPrice, false, home, deviceList);
-		orderRequestService.saveOrder(order);
+        orderRequestService.saveOrder(order);
         user.getOrderList().add(order);
         userService.saveUser(user);
-		log.info("Oreder created");
+        log.info("Oreder created");
         return "redirect:see";
-	}
+    }
 
     @RequestMapping("/charts")
     public String charts(Model model, Principal principal) {
@@ -219,16 +224,16 @@ public class UserDashboardController implements CurrentUserInfo {
     }
 
     @RequestMapping("/see")
-    public String see(Model model, Principal principal) {    		
-    		//del usuario tengo el id
-    		User user =  userService.findOneById(getIdFromPrincipalName(principal.getName()));
-    		log.info("usuario pillado"+ user);
-    		//con el id del usuario obtengo el id de su casa en una lista por si tiene mas de 1
-    		List<Home> homeList= homeService.getHomesFromUser(user);
-    		//Home homeUser = homeService.findOneById(user.getId());
-    		List<OrderRequest> orderRequestList = orderRequestService.findNotCompletedOrders(homeList);
-    		model.addAttribute("orderList", orderRequestList);
-    		model.addAttribute("userHome", homeList);
+    public String see(Model model, Principal principal) {
+        //del usuario tengo el id
+        User user = userService.findOneById(getIdFromPrincipalName(principal.getName()));
+        log.info("usuario pillado" + user);
+        //con el id del usuario obtengo el id de su casa en una lista por si tiene mas de 1
+        List<Home> homeList = homeService.getHomesFromUser(user);
+        //Home homeUser = homeService.findOneById(user.getId());
+        List<OrderRequest> orderRequestList = orderRequestService.findNotCompletedOrders(homeList);
+        model.addAttribute("orderList", orderRequestList);
+        model.addAttribute("userHome", homeList);
     		
     		/*Home home = homeService.findOneById(homeList.get(0));   		
     		List<OrderRequest> orderRequest = user.getOrderList();
@@ -236,16 +241,16 @@ public class UserDashboardController implements CurrentUserInfo {
     		for (User user2 : userHomeList) {
 				orderRequest.add(orderRequestService.finOneById(homeUser.getId()));
 			}*/
-    		
+
         //List<OrderRequest> orderRequestList;
         //List<User> userHomeList;
         /*for () {
 			orderRequest.add(orderRequestService.findOrderByHomeId(home.getId()));
         	
 		}*/
-       
+
         model.addAttribute("user", user);
-        
+
         return "dashboard/see";
     }
 
