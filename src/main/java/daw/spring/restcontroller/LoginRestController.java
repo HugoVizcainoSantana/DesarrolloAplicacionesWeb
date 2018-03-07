d3a6edbe994af1898e4791b7dc770b94ef74ce65
Jorge Galindo Peña <jorgape225@gmail.com>
2018-03-07 20:27:37 +0100

    Login and logout with API Rest working
    Login and logout with API Rest working and returning a Map with key boolean and the user itself

--- src/main/java/daw/spring/restcontroller/LoginRestController.java
+++ src/main/java/daw/spring/restcontroller/LoginRestController.java
@@ -2,10 +2,7 @@ package daw.spring.restcontroller;
 
 import daw.spring.component.UserComponent;
 import daw.spring.model.User;
-import daw.spring.service.ProductService;
 import daw.spring.utilities.ApiRestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,43 +15,36 @@ import java.util.Map;
 @ApiRestController
 public class LoginRestController {
 
-    // TODO add interfaces for represent the api here and @JsonView (using interfaces) on attributes
-
-    private static final Logger log = LoggerFactory.getLogger(LoginRestController.class);
     private final UserComponent userComponent;
-    private final ProductService productService;
 
     @Autowired
-    public LoginRestController(UserComponent userComponent, ProductService productService) {
+    public LoginRestController(UserComponent userComponent) {
         this.userComponent = userComponent;
-        this.productService = productService;
     }
 
     @RequestMapping(value = "/login")
-    public ResponseEntity<User> login() {
+    public ResponseEntity<Map<Boolean, User>> login() {
+
+        Map<Boolean, User> map = new HashMap<>();
+        map.put(true, userComponent.getLoggedUser());
 
         if (!userComponent.isLoggedUser()) {
-            log.info("NO LOGGED :(");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         } else {
-            User loggedUser = userComponent.getLoggedUser();
-            log.info("LOGGED AS " + loggedUser.getEmail());
-            return new ResponseEntity<>(loggedUser, HttpStatus.OK);
+            return new ResponseEntity<>(map, HttpStatus.OK);
         }
     }
 
     @RequestMapping("/logout")
     public ResponseEntity<Map<Boolean, User>> logout(HttpSession session) {
 
-        User loggedUser = userComponent.getLoggedUser();
         Map<Boolean, User> map = new HashMap<>();
-        map.put(true, loggedUser);
+        map.put(true, userComponent.getLoggedUser());
 
         if (!userComponent.isLoggedUser()) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         } else {
             session.invalidate();
-            log.info("LOGOUT DONE");
             return new ResponseEntity<>(map, HttpStatus.OK);
         }
     }
