82dd62b57c0110a8ebc797a768fbb4404f168b75
Jorge Galindo Peña <jorgape225@gmail.com>
2018-03-10 20:39:15 +0100

    Added register api method
    Added register user and register admin api method in LoginRestController

--- src/main/java/daw/spring/restcontroller/LoginRestController.java
+++ src/main/java/daw/spring/restcontroller/LoginRestController.java
@@ -1,12 +1,16 @@
 package daw.spring.restcontroller;
 
 import daw.spring.component.UserComponent;
+import daw.spring.model.Roles;
 import daw.spring.model.User;
+import daw.spring.service.UserService;
 import daw.spring.utilities.ApiRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
@@ -16,13 +20,15 @@ import java.util.Map;
 public class LoginRestController {
 
     private final UserComponent userComponent;
+    private final UserService userService;
 
     @Autowired
-    public LoginRestController(UserComponent userComponent) {
+    public LoginRestController(UserComponent userComponent, UserService userService) {
         this.userComponent = userComponent;
+        this.userService = userService;
     }
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ResponseEntity<Map<Boolean, User>> login() {
 
         Map<Boolean, User> map = new HashMap<>();
@@ -35,7 +41,7 @@ public class LoginRestController {
         }
     }
 
-    @RequestMapping(value = "/logout")
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public ResponseEntity<Map<Boolean, User>> logout(HttpSession session) {
 
         Map<Boolean, User> map = new HashMap<>();
@@ -49,4 +55,47 @@ public class LoginRestController {
         }
     }
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<Map<Boolean, User>> register(@RequestBody User user) {
+
+        Map<Boolean, User> map = new HashMap<>();
+
+        if (userComponent.isLoggedUser()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        try {
+            User newUser = new User(user.getFirstName(), user.getLastName(), user.getEmail(),
+                    user.getPasswordHash(), user.getHomeList(), user.getPhone(),
+                    user.getNotificationList(), user.getPhoto(), user.getOrderList(), Roles.USER.getRoleName());
+
+            userService.saveUser(newUser);
+            map.put(true, newUser);
+
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @RequestMapping(value = "/registerAdmin", method = RequestMethod.POST)
+    public ResponseEntity<Map<Boolean, User>> registerAdmin(@RequestBody User user) {
+
+        Map<Boolean, User> map = new HashMap<>();
+
+        if (userComponent.isLoggedUser()) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        try {
+            User newUser = new User(user.getFirstName(), user.getLastName(), user.getEmail(),
+                    user.getPasswordHash(), user.getHomeList(), user.getPhone(),
+                    user.getNotificationList(), user.getPhoto(), user.getOrderList(), Roles.ADMIN.getRoleName());
+
+            userService.saveUser(newUser);
+            map.put(true, newUser);
+
+            return new ResponseEntity<>(map, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
