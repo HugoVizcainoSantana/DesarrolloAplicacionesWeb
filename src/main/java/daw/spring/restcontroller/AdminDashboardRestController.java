93615d6ed474691b1590d047614cc82b0e1fbc30
CVelazquezURJC <c.velazquezs@alumnos.urjc.es>
2018-03-13 09:28:15 +0100

    fixed url rest controller

--- src/main/java/daw/spring/restcontroller/AdminDashboardRestController.java
+++ src/main/java/daw/spring/restcontroller/AdminDashboardRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/adminDashboard")
+@RequestMapping("/api")
 public class AdminDashboardRestController {
 
     private final ProductService productService;
